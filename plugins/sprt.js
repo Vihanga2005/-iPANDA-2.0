a50dff7eead1ac8415375d08fe37be6a1ffa3746
ＮＯＩＺ Ξ <86784191+Zenoixnoize@users.noreply.github.com>
2022-02-22 16:12:25 +0530

    Update sprt.js

--- plugins/sprt.js
+++ plugins/sprt.js
@@ -1,15 +1,60 @@
 const Asena = require('../events');
+
+
+2
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+
+
+3
 const fs = require('fs');
+
+
+4
 const axios = require('axios');
 
-Asena.addCommand({pattern: 'project', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
 
+5
+
+
+
+6
+Asena.addCommand({pattern: 'ipanda', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+
+7
+
+
+
+8
     var r_text = new Array ();    
-r_text[0] = "*Garfield bot project was coded by Tharindu Liyanage                            Special thanks                                 🏆@Bailays                                     🏆@afnanpalikkal                               🏆@yusufunta                                  🏆@Farhan                                     🏆@hasindu                                    🏆@Lasindunethsara*";
-r_text[1] = "*Garfield bot project was coded by Tharindu Liyanage                            Special thanks                                 🏆@Bailays                                     🏆@afnanpalikkal                               🏆@yusufunta                                  🏆@Farhan                                     🏆@hasindu                                    🏆@Lasindunethsara*";
+
+
+9
+r_text[0] = "*iPANDA BOT INSPIRED BY GARFIELD 3.0                            Special thanks                                 🐼@Bailays                                     🐼@afnanpalikkal                               🐼@yusufunta                                  🐼@Farhan                                     🐼@hasindu                                    🐼@Lasindunethsara*";
+
+
+10
+r_text[1] = "*iPANDA BOT INSPIRED BY GARFIELD 3.0                             Special thanks                                 🐼@Bailays                                     🐼@afnanpalikkal                               🐼@yusufunta                                  🐼@Farhan                                     🐼@hasindu                                    🐼@Lasindunethsara*                         🐼@Tharindu Liyanage";
+
+
+11
 var i = Math.floor(2*Math.random())
 
+
+12
+
+
+
+13
 await message.sendMessage(r_text[i]);
 
+
+14
+
+
+
+15
 }));
+
+
+16
